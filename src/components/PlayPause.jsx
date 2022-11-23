549a2440fd571ac03c5f9feb15812bb0e636610f
afisman <afisman@gmail.com>
2022-11-23 11:28:18 +0100

    app upload

---
+++ src/components/PlayPause.jsx
@@ -0,0 +1,17 @@
+import { FiPauseCircle, FiPlayCircle } from 'react-icons/fi';
+const PlayPause = ({ isPlaying, activeSong, song, handlePause, handlePlay }) => (isPlaying && activeSong?.title === song.title ? (
+  <FiPauseCircle
+    size={35}
+    className='text-gray-300'
+    onClick={handlePause}
+  />
+) : (
+  <FiPlayCircle
+    size={35}
+    className='text-gray-300'
+    onClick={handlePlay}
+  />
+)
+)
+
+export default PlayPause;
---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+
+export const shazamCoreApi = createApi({
+    reducerPath: 'shazamCoreApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+        prepareHeaders: (headers) => {
+            headers.set('X-RapidAPI-Key', 'de730a1330mshdeb36b25b473e92p1f6dfajsn85dfcfef76ee');
+
+            return headers
+        }
+    }),
+    endpoints: (builder) => ({
+        getTopCharts: builder.query({ query: () => '/charts/world' }),
+    })
+});
+
+export const {
+    useGetTopChartsQuery,
+} = shazamCoreApi
